39aab20118245deffda02fe0dee2650808c811c9
JSIFR8E <jasiabra@profile.es>
2018-11-14 11:20:38 +0100

    Se incluyr Mockito para el testeo de dependencias de ZendeskService

--- src/test/java/com/mycorp/RealizarSimulacionTest.java
+++ src/test/java/com/mycorp/RealizarSimulacionTest.java
@@ -1,31 +1,53 @@
 package com.mycorp;
 
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
+
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.runners.MockitoJUnit44Runner;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import com.mycorp.support.MensajeriaService;
+import com.mycorp.support.ZendeskHelper;
 
 import junit.framework.TestCase;
+import portalclientesweb.ejb.interfaces.PortalClientesWebEJBRemote;
+import util.datos.PolizaBasico;
 import util.datos.UsuarioAlta;
 
 
 /**
  * Unit test for simple App.
  */
-public class RealizarSimulacionTest extends TestCase {
+@RunWith(MockitoJUnit44Runner.class)
+public class RealizarSimulacionTest {
 
 	private static String NUM_TARJETA = "NUM_TARJETA";
 	private static String NUM_POLIZA = "12345";
 	private static String NUM_ACRED = "12345";
 
-
+	@InjectMocks
 	ZendeskService zendeskService = new ZendeskService();
+	
+	@Mock
+    private PortalClientesWebEJBRemote portalclientesWebEJBRemote;
+	@Mock
+    MensajeriaService emailService;
+    @Mock
+    ZendeskHelper zendeskHelper;
 	
     /**
      * Rigourous Test :-)
      */
     @Test
     public void testApp() {
-        assertTrue( true );
+       // assertTrue( true );
     }
     
     @Test
@@ -33,7 +55,7 @@ public class RealizarSimulacionTest extends TestCase {
     	UsuarioAlta usuarioAlta = new UsuarioAlta();
     	String userAgent = "AGENT";
     	zendeskService.altaTicketZendesk(usuarioAlta, userAgent);
-        assertTrue( true );
+       // assertTrue( true );
     }
     
     @Test
@@ -41,8 +63,12 @@ public class RealizarSimulacionTest extends TestCase {
     	UsuarioAlta usuarioAlta = new UsuarioAlta();
     	usuarioAlta.setNumPoliza(NUM_POLIZA);
     	String userAgent = "AGENT";
+    	
+        when(zendeskHelper.createPolizaBasicoFromUsuarioAlta(any(UsuarioAlta.class))).thenReturn(new PolizaBasico());
+
+    	
     	zendeskService.altaTicketZendesk(usuarioAlta, userAgent);
-        assertTrue( true );
+        //assertTrue( true );
     }
     
     @Test
@@ -51,7 +77,7 @@ public class RealizarSimulacionTest extends TestCase {
     	usuarioAlta.setNumTarjeta(NUM_TARJETA);
     	String userAgent = "AGENT";
     	zendeskService.altaTicketZendesk(usuarioAlta, userAgent);
-        assertTrue( true );
+        //assertTrue( true );
     }
 
 }
